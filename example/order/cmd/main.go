6464480719b7c7e92bd9b3e7a39878c1779c48e9
Morgan Hallgren <morgan.hallgren@gmail.com>
2024-12-27 15:42:03 +0100

    order example

--- example/order/cmd/main.go
+++ example/order/cmd/main.go
@@ -10,19 +10,22 @@ import (
 	"github.com/hallgren/eventsourcing/example/order"
 )
 
+type Order struct {
+	DiscountAmount uint
+	Total          uint
+}
+
 func main() {
 	es := memory.Create()
 	repo := eventsourcing.NewEventRepository(es)
 	repo.Register(&order.Order{})
 
-	orderMap := make(map[string]string)
-	startedCount := 0
+	ongoingOrders := make(map[string]*Order)
 	completedCount := 0
-	discountAppliedCount := 0
 	moneyMade := 0
+	totalDiscountAmount := 0
 
 	go func() {
-
 		i := 0
 		for {
 			o, err := order.Create(100)
@@ -33,13 +36,17 @@ func main() {
 			if err != nil {
 				panic(err)
 			}
+			// 33% of orders remove discount
+			if i%3 == 0 {
+				o.RemoveDiscount()
+			}
 			err = o.Pay(80)
 			if err != nil {
 				panic(err)
 			}
-			// 50% of orders are not completed
-			if i%2 == 0 {
-				err = o.Pay(10)
+			// 25% of orders are not completed
+			if i%4 == 0 {
+				err = o.Pay(o.Outstanding)
 				if err != nil {
 					panic(err)
 				}
@@ -54,23 +61,31 @@ func main() {
 	}()
 
 	for {
+		// setup how the projection will handle events and build the read model
 		p := repo.Projections.Projection(es.All(0, 3), func(e eventsourcing.Event) error {
-			if e.Reason() == "Created" {
-				orderMap[e.AggregateID()] = "Created"
-				startedCount++
-			} else if e.Reason() == "Completed" {
-				completedCount++
-				delete(orderMap, e.AggregateID())
-			} else if e.Reason() == "DiscountApplied" {
-				discountAppliedCount++
-			} else if e.Reason() == "Paid" {
-				switch event := e.Data().(type) {
-				case *order.Paid:
-					moneyMade += int(event.Amount)
+			switch event := e.Data().(type) {
+			// When an order is created add it to an order map
+			case *order.Created:
+				ongoingOrders[e.AggregateID()] = &Order{
+					Total: event.Total,
 				}
+			// When order is complete add the discount amount to a sum also store the total amount of completed orders
+			case *order.Completed:
+				completedCount++
+				totalDiscountAmount += int(ongoingOrders[e.AggregateID()].DiscountAmount)
+				// delete the order from the map
+				delete(ongoingOrders, e.AggregateID())
+			// Store the discount amount in the order in the map
+			case *order.DiscountApplied:
+				ongoingOrders[e.AggregateID()].DiscountAmount = ongoingOrders[e.AggregateID()].Total - event.Total
+			// If the discount is removed reset the discount amount in the orders map
+			case *order.DiscountRemoved:
+				ongoingOrders[e.AggregateID()].DiscountAmount = 0
+			// If a payment is made store the value
+			case *order.Paid:
+				moneyMade += int(event.Amount)
 			}
-
-			fmt.Println(startedCount, completedCount, discountAppliedCount, moneyMade)
+			fmt.Println("active order:", len(ongoingOrders), "completed orders:", completedCount, "money made:", moneyMade, "total discount amount:", totalDiscountAmount)
 			return nil
 		})
 		p.Run(context.Background(), time.Second*2)
