e386bbfa73e7c38f1967243ec52cf7d628e3c655
Morgan Hallgren <morgan.hallgren@gmail.com>
2025-06-27 14:00:06 +0200

    NVARCHAR(MAX) -> NVARCHAR(255)

--- eventstore/sql/sqlserver.go
+++ eventstore/sql/sqlserver.go
@@ -13,11 +13,11 @@ import (
 
 const createTableSQLServer = `CREATE TABLE events (
     seq INT IDENTITY(1,1) PRIMARY KEY,
-    id NVARCHAR(MAX) NOT NULL,
+    id NVARCHAR(255) NOT NULL,
     version INT,
-    reason NVARCHAR(MAX),
-    type NVARCHAR(MAX),
-    timestamp NVARCHAR(MAX),
+    reason NVARCHAR(255),
+    type NVARCHAR(255),
+    timestamp NVARCHAR(255),
     data VARBINARY(MAX),
     metadata VARBINARY(MAX),
     CONSTRAINT uq_events UNIQUE (id, type, version)
