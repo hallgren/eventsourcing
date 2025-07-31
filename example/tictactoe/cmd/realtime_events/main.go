b3a80e6a4c06d5c1f71f5858e8bc8d63da45b82e
Morgan Hallgren <morgan.hallgren@gmail.com>
2025-08-01 01:01:03 +0200

    realtime tictactoe example

--- example/tictactoe/cmd/realtime_events/main.go
+++ example/tictactoe/cmd/realtime_events/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 
 	"github.com/hallgren/eventsourcing"
 	"github.com/hallgren/eventsourcing/aggregate"
@@ -10,16 +11,38 @@ import (
 )
 
 func main() {
+	m := map[string]int{
+		"Draw": 0,
+		"X":    0,
+		"O":    0,
+	}
 	es := memory.Create()
 	aggregate.Register(&tictactoe.Game{})
 	aggregate.SaveHook(func(events []eventsourcing.Event) {
-		fmt.Println(events, len(events))
-	},
-		&tictactoe.Game{},
-	)
+		lastEvent := events[len(events)-1]
+		switch lastEvent.Data().(type) {
+		case *tictactoe.Draw:
+			m["Draw"]++
+		case *tictactoe.XWon:
+			m["X"]++
+		case *tictactoe.OWon:
+			m["O"]++
+		}
+	}, &tictactoe.Game{})
 
+	for i := 0; i < 10; i++ {
+		game := PlayGame()
+		aggregate.Save(es, game)
+	}
+	fmt.Println(m)
+}
+
+func PlayGame() *tictactoe.Game {
 	game := tictactoe.NewGame()
-	game.PlayMove(1, 2)
-	aggregate.Save(es, game)
-	fmt.Println("hej")
+	for !game.Done() {
+		x := rand.Intn(3)
+		y := rand.Intn(3)
+		game.PlayMove(x, y)
+	}
+	return game
 }
