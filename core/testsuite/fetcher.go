c107e52ba97bde3fc85032fc85d4779aa8a4bb89
Morgan Hallgren <morgan.hallgren@gmail.com>
2025-11-25 21:19:39 +0100

    core fetcher test

--- core/testsuite/fetcher.go
+++ core/testsuite/fetcher.go
@@ -1,6 +1,7 @@
 package testsuite
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hallgren/eventsourcing/core"
@@ -13,38 +14,50 @@ func TestFetcherAll(t *testing.T, es core.EventStore, fetcher core.Fetcher) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	events2 := testEventOtherAggregate(AggregateID())
-	err = es.Save([]core.Event{events2})
-	if err != nil {
-		t.Fatal(err)
-	}
-	events3 := testEventsPartTwo(aggregateID)
-	err = es.Save(events3)
-	if err != nil {
-		t.Fatal(err)
-	}
 
 	iter, err := fetcher()
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer iter.Close()
+	err = verify(iter, 0, len(events))
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	globalVersion := core.Version(0)
-	expCount := len(events) + 1 + len(events3)
+	events2 := testEventsPartTwo(aggregateID)
+	err = es.Save(events2)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// run fetcher second time - should not restart from first event
+	iter2, err := fetcher()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer iter2.Close()
+	err = verify(iter, core.Version(len(events)), len(events2))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func verify(iter core.Iterator, globalVersion core.Version, expCount int) error {
 	count := 0
 	for iter.Next() {
 		count++
 		event, err := iter.Value()
 		if err != nil {
-			t.Fatal(err)
+			return err
 		}
 		if event.GlobalVersion <= globalVersion {
-			t.Fatalf("event global version (%q) is lower than previos event %q", event.GlobalVersion, globalVersion)
+			return fmt.Errorf("event global version (%q) is lower than previos event %q", event.GlobalVersion, globalVersion)
 		}
 		globalVersion = event.GlobalVersion
 	}
 	if count != expCount {
-		t.Fatalf("expected %d events got %d", expCount, count)
+		return fmt.Errorf("expected %d events got %d", expCount, count)
 	}
+	return nil
 }
