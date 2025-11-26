50e8e691c8fa90e28c82b61b224462756e5f18ff
Morgan Hallgren <morgan.hallgren@gmail.com>
2025-11-26 21:38:07 +0100

    fix(core): fetcher testsuite (#217)
    * fetcher testsuite
    
    Will test that the Fetcher is able to fetch event multiple times
    and don't restart from the starting position.

--- core/testsuite/fetcher.go
+++ core/testsuite/fetcher.go
@@ -1,25 +1,18 @@
 package testsuite
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hallgren/eventsourcing/core"
 )
 
-func TestFetcherAll(t *testing.T, es core.EventStore, fetcher core.Fetcher) {
+func TestFetcher(t *testing.T, es core.EventStore, fetcher core.Fetcher) {
+	globalVersion := core.Version(0)
 	aggregateID := AggregateID()
+
 	events := testEvents(aggregateID)
 	err := es.Save(events)
-	if err != nil {
-		t.Fatal(err)
-	}
-	events2 := testEventOtherAggregate(AggregateID())
-	err = es.Save([]core.Event{events2})
-	if err != nil {
-		t.Fatal(err)
-	}
-	events3 := testEventsPartTwo(aggregateID)
-	err = es.Save(events3)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -29,22 +22,52 @@ func TestFetcherAll(t *testing.T, es core.EventStore, fetcher core.Fetcher) {
 		t.Fatal(err)
 	}
 	defer iter.Close()
+	err = verify(iter, globalVersion, events)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// set the globalVersion to the length of the events as not all event stores update the
+	// globalVersion on the events after they are saved
+	globalVersion = core.Version(len(events))
 
-	globalVersion := core.Version(0)
-	expCount := len(events) + 1 + len(events3)
-	count := 0
+	events2 := testEventsPartTwo(aggregateID)
+	err = es.Save(events2)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// run fetcher second time - should not restart from first event
+	iter2, err := fetcher()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer iter2.Close()
+	err = verify(iter2, globalVersion, events2)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+// verifies that the events from the iterator has a globalversion higher than sent in globalVersion and that
+// the events are the same from the iterator and sent in event.
+func verify(iter core.Iterator, globalVersion core.Version, expEvents []core.Event) error {
+	i := 0
 	for iter.Next() {
-		count++
 		event, err := iter.Value()
 		if err != nil {
-			t.Fatal(err)
+			return err
+		}
+		if event.Version != expEvents[i].Version && event.AggregateID != expEvents[i].AggregateID {
+			return fmt.Errorf("missmatch in expected event version %q and actual version %q, expected aggregate id %q actual aggregate id %q", expEvents[i].Version, event.Version, expEvents[i].AggregateID, event.AggregateID)
 		}
 		if event.GlobalVersion <= globalVersion {
-			t.Fatalf("event global version (%q) is lower than previos event %q", event.GlobalVersion, globalVersion)
+			return fmt.Errorf("event global version (%q) is lower than previos event %q", event.GlobalVersion, globalVersion)
 		}
 		globalVersion = event.GlobalVersion
+		i++
 	}
-	if count != expCount {
-		t.Fatalf("expected %d events got %d", expCount, count)
+	if i != len(expEvents) {
+		return fmt.Errorf("expected %d events got %d", len(expEvents), i)
 	}
+	return nil
 }
